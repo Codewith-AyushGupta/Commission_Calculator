17b3a6738dd1170031e979d3ca6badba354abc43
Ayush Gupta <ayush.gupta@nubystech.com>
2023-03-23 21:30:35 +0530

    batchAndUI

--- .sfdx/typings/lwc/apex/MyCommissionController.d.ts
+++ .sfdx/typings/lwc/apex/MyCommissionController.d.ts
@@ -2,5 +2,5 @@ declare module "@salesforce/apex/MyCommissionController.getLoggedInUser" {
   export default function getLoggedInUser(): Promise<any>;
 }
 declare module "@salesforce/apex/MyCommissionController.getMonthlyBreakDown" {
-  export default function getMonthlyBreakDown(param: {month: any, userName: any}): Promise<any>;
+  export default function getMonthlyBreakDown(param: {userDetail: any}): Promise<any>;
 }
--- force-app/main/default/aura/MC_Commissions/MC_CommissionsController.js
+++ force-app/main/default/aura/MC_Commissions/MC_CommissionsController.js
@@ -1,38 +1,49 @@
 ({
     doInit : function(component, event, helper) {
+        //console.log('entry doInit');
+        component.set('v.monthlyBreakDown',[]);
         var currentDay = new Date();
-        let months =[{'label': 'January', 'value': '1'},
-                     {'label': 'February', 'value': '2'},
-                     {'label': 'March', 'value': '3'},
-                     {'label': 'April', 'value': '4'},
-                     {'label': 'May', 'value': '5'},
-                     {'label': 'June', 'value': '6'},
-                     {'label': 'July', 'value': '7'},
-                     {'label': 'August', 'value': '8'},
-                     {'label': 'September', 'value': '9'},
-                     {'label': 'October', 'value': '10'},
-                     {'label': 'November', 'value': '11'},
-                     {'label': 'December', 'value': '12'}];
-        var year = currentDay.getFullYear();
+        let Tablemonths =[];
+        let months =[{'label': 'January', 'value': '-Jan-'},
+                     {'label': 'February', 'value': '-Feb-'},
+                     {'label': 'March', 'value': '-Mar'},
+                     {'label': 'April', 'value': '-Apr-'},
+                     {'label': 'May', 'value': '-May-'},
+                     {'label': 'June', 'value': '-Jun-'},
+                     {'label': 'July', 'value': '-Jul-'},
+                     {'label': 'August', 'value': '-Aug-'},
+                     {'label': 'September', 'value': '-Sep-'},
+                     {'label': 'October', 'value': '-Oct-'},
+                     {'label': 'November', 'value': '-Nov-'},
+                     {'label': 'December', 'value': '-Dec-'}];
         var month = currentDay.getMonth() +1;
-        console.log('year'+year);
-        let yearList =[{'label': year , 'value': year},
-                       {'label': year-1, 'value': year-1},
-                       {'label': year-2, 'value': year-2}];
-        component.set('v.selectedYear',currentDay.getFullYear());
-        component.set('v.monthList',months);
-        component.set('v.selectedMonth',month);
-        component.set('v.yearList',yearList);
+        for(let temp in months){
+            if(months[temp].value ==months[month].value){
+                break;
+            }
+            else{
+                Tablemonths.push(months[temp])
+            }
+        }
+        component.set('v.monthList',Tablemonths);
+        component.set('v.masterMonthList',months);
+        component.set('v.selectedMonth',months[month-1].value);
+        //console.log('exit doInit');
     },
     handleUserChanges : function(component, event, helper) {
+        //  console.log('entry handleUserChanges');
         helper.getBreakDownData(component, event, helper);  
-        helper.updateRecordThroughYear(component,event,helper);    
-        helper.updateRecordThroughMonth(component,event,helper);    
+        //   console.log('Exit handleUserChanges');
     },
-    updaterecordThroughMonth : function(component, event, helper) {
+    updaterecordThroughMonth : function(component, event, helper) {      
+        //  console.log('entry updaterecordThroughMonth');
+        component.set('v.spinner',true);
         helper.updateRecordThroughMonth(component,event,helper);    
+        //  console.log('exit updaterecordThroughMonth');
     },
     updaterecordThroughYear : function(component, event, helper) {
+        //  console.log('entry updaterecordThroughYear');
         helper.updateRecordThroughYear(component,event,helper);    
+        //  console.log('exit updaterecordThroughYear');
     },
 })
